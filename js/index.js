6688cfc57a3b1262f7b48d7d5426bef49b1f7d59
Bonnie <73203899+Bonnie39@users.noreply.github.com>
2023-12-26 12:45:35 -0600

    saves user-added repos

--- js/index.js
+++ js/index.js
@@ -9,46 +9,82 @@ function addLink() {
 
         fetch(link)
             .then(response => response.json())
-            .then(data => displayLink(data, link))
+            .then(data => {
+                // Save the link as a cookie
+                saveLinkAsCookie(link);
+
+                // Display the link
+                displayLink(data, link);
+            })
             .catch(error => console.error('Error fetching data:', error));
     }
 }
 
+function saveLinkAsCookie(link) {
+    const savedLinks = getSavedLinksFromCookie() || new Set();
+    savedLinks.add(link);
+
+    // Set the cookie with the links array
+    document.cookie = `jsonLinks=${encodeURIComponent(JSON.stringify(Array.from(savedLinks)))}; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/`;
+}
+
+function getSavedLinksFromCookie() {
+    // Retrieve saved links from the cookie
+    const cookies = document.cookie.split(';');
+    for (const cookie of cookies) {
+        const [name, value] = cookie.trim().split('=');
+        if (name === 'jsonLinks') {
+            return new Set(JSON.parse(decodeURIComponent(value)));
+        }
+    }
+    return null;
+}
+
+// Load saved links when the page is loaded
+document.addEventListener('DOMContentLoaded', function () {
+    const savedLinks = getSavedLinksFromCookie();
+    if (savedLinks) {
+        savedLinks.forEach(link => {
+            fetch(link)
+                .then(response => response.json())
+                .then(data => displayLink(data, link))
+                .catch(error => console.error('Error fetching data:', error));
+        });
+    }
+});
 
 function loadRepo(url) {
     fetch(url)
-      .then(response => response.json())
-      .then(data => displayLink(data, url))
-      .catch(error => console.error('Error fetching data:', error));
+        .then(response => response.json())
+        .then(data => displayLink(data, url))
+        .catch(error => console.error('Error fetching data:', error));
 }
-  
+
 function displayLink(data, url) {
     var linksContainer = document.getElementById("links-container");
     var linkItem = document.createElement("div");
     linkItem.className = "link-item";
-  
+
     // Set custom data attribute to store the repo link
     linkItem.setAttribute("data-repo-link", url);
 
     linkItem.addEventListener('click', function (e) {
         e.preventDefault(); // Prevent default behavior for click event
         this.classList.toggle('active');
-        //alert(this.getAttribute("data-repo-link"));
         var link = this.getAttribute('data-repo-link');
-        //history.replaceState({}, document.title, `/repo/?query=${encodeURIComponent(link)}`);
         window.location.href = `/repo/index.html?url=${encodeURIComponent(link)}`;
     });
-  
+
     // Adding the link-item-content for better organization
     var linkItemContent = document.createElement("div");
     linkItemContent.className = "link-item-content";
-  
+
     // Displaying the iconURL on the left
     var iconImage = document.createElement("img");
     iconImage.src = data.iconURL;
     iconImage.alt = "Source Icon";
     linkItemContent.appendChild(iconImage);
-  
+
     // Displaying the name of the source
     var sourceName = document.createElement("span");
     sourceName.textContent = data.name;
@@ -57,15 +93,13 @@ function displayLink(data, url) {
     sourceName.style.fontSize = "16px";
     sourceName.style.verticalAlign = "middle";
     linkItemContent.appendChild(sourceName);
-  
+
     linkItem.appendChild(linkItemContent);
-  
+
     // Adding the link-item-button
     var linkItemButton = document.createElement("div");
     linkItemButton.className = "link-item-button";
     linkItem.appendChild(linkItemButton);
-  
+
     linksContainer.appendChild(linkItem);
 }
-
-  
